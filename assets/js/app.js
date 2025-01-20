b45a5cb408ead43e078e3fd156927c92f1388fa9
Piero Nicolini <naturalnen@gmail.com>
2025-01-19 23:12:50 -0300

    add filter method

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,99 +1,97 @@
-let listaTareas = [{
+let listaTareas = [
+  {
     id: 1,
     name: "despertar",
-    checked: false
-},{
+    checked: false,
+  },
+  {
     id: 2,
-    name: "comer",
-    checked: false
-},{
+    name: "desayunar",
+    checked: false,
+  },
+  {
     id: 3,
-    name: "ducharse",
-    checked: false
-},];
+    name: "estudiar",
+    checked: false,
+  },
+];
 
-let lista = document.getElementById('lista');
-let boton = document.getElementById('boton');
-let input = document.getElementById('input');
-let total = document.getElementById('total');
-let realizadas = document.getElementById('realizadas');
+let lista = document.getElementById("lista");
+let boton = document.getElementById("boton");
+let input = document.getElementById("input");
+let total = document.getElementById("total");
+let realizadas = document.getElementById("realizadas");
 let tareasTotal = 0;
 let tareasListas = 0;
 
-
 function contadorTareas() {
-    tareasTotal = listaTareas.length;
-    total.innerText = `${tareasTotal}`
-    let tareasCheck = 0;
-    for (let task of listaTareas){
-        if (task.checked){
-            tareasCheck ++;
-        }
-    }
-    realizadas.innerText = `${tareasCheck}`;
+  tareasTotal = listaTareas.length;
+  total.innerText = `${tareasTotal}`;
+  let cuantosChecked = listaTareas.filter((tareas) => tareas.checked == true);
+  realizadas.innerText = `${cuantosChecked.length}`;
 }
 
 let checkTrue = (id) => `
 <input checked type="checkbox" onchange="cambiarEstado(${id})">
-`
+`;
 let checkFalse = (id) => `
 <input type="checkbox" onchange="cambiarEstado(${id})">
-`
+`;
 function cambiarEstado(id) {
-    let estado = listaTareas.find(estado => estado.id == id);
-    if (estado) {
-        estado.checked = !estado.checked;
-    }
-    renderizarLista();
+  let estado = listaTareas.find((estado) => estado.id == id);
+  if (estado) {
+    estado.checked = !estado.checked;
+  }
+  renderizarLista();
 }
 
-
-function renderizarLista(){
-    actualizarIds();
-    let template= ``
-    for(let task of listaTareas) {
-        template +=`<li>
+function renderizarLista() {
+  actualizarIds();
+  let template = ``;
+  for (let task of listaTareas) {
+    template += `<li>
         <div class="datos">
-        <p id="${task.id}" class="${task.checked?'tachado':''}">${task.id}</p>
-        <p id="${task.id}" class="${task.checked?'tachado':''}">${task.name}</p>
+        <p id="${task.id}" class="${task.checked ? "tachado" : ""}">${
+      task.id
+    }</p>
+        <p id="${task.id}" class="${task.checked ? "tachado" : ""}">${
+      task.name
+    }</p>
         </div><div class="triggers">
-        ${task.checked?checkTrue(task.id):checkFalse(task.id)}
+        ${task.checked ? checkTrue(task.id) : checkFalse(task.id)}
         <a onclick="borrarPorId(${task.id})">X</a></div>
-        </li>`
-    }
-    lista.innerHTML = template;
-    contadorTareas()
+        </li>`;
+  }
+  lista.innerHTML = template;
+  contadorTareas();
 }
 
 function crearTarea(task) {
-    let tarea = {};
-    tarea.id = listaTareas.length + 1;
-    tarea.name = `${task}`;
-    tarea.checked = false,
-    listaTareas.push(tarea)
-    renderizarLista()
+  let tarea = {};
+  tarea.id = listaTareas.length + 1;
+  tarea.name = `${task}`;
+  (tarea.checked = false), listaTareas.push(tarea);
+  renderizarLista();
 }
 
 function actualizarIds() {
-    listaTareas = listaTareas.map((tarea, index) => {
-        return {...tarea, id: index + 1}
-    });
+  listaTareas = listaTareas.map((tarea, index) => {
+    return { ...tarea, id: index + 1 };
+  });
 }
 
-function borrarPorId(id){
-    let indiceTask = listaTareas.findIndex((tarea) => tarea.id == id);
-    if (indiceTask != -1) {
-        listaTareas.splice(indiceTask, 1)
-    }
-    renderizarLista()
+function borrarPorId(id) {
+  let indiceTask = listaTareas.findIndex((tarea) => tarea.id == id);
+  listaTareas.splice(indiceTask, 1);
+  renderizarLista();
 }
 
-boton.addEventListener('click', () => {
-    let tarea = input.value;
-    if (tarea.trim() != "") {
-        crearTarea(tarea.trim());
-        input.value="";
-    }
-})
+boton.addEventListener("click", () => {
+  let tarea = input.value;
+  if (tarea.trim() != "") {
+    crearTarea(tarea.trim());
+    input.value = "";
+  }
+});
 
 renderizarLista();
